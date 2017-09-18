cc5d6905421ce10d920b3430f0ec2b0732f17d12
kasanir <kasanir@gmail.com>
2017-09-18 10:48:35 +0400

    Update BreakBox.lua

--- BreakBox.lua
+++ BreakBox.lua
@@ -1,88 +1,17 @@
 -- Параметры источника данных
 CLASS_CODE  = "SPBFUT";     -- Класс бумаги
-SEC_CODE = "RIH7";          -- Код бумаги
-TIME_FRAME = INTERVAL_M1;   -- Таймфрейм
+SEC_CODE    = "RIH7";          -- Код бумаги
+TIME_FRAME  = INTERVAL_M1;   -- Таймфрейм
 -- Параметры стратегии
-LotsLong   = 1;       -- Количество лотов для входа в лонг
-LotsShort   = 1;      -- Количество лотов для входа в шорт
-TimeStart  = 0;       -- Время начала работы стратегии
-TimeFinish  = 0;      -- Время окончания работы стратегии
+LotsLong      = 1;       -- Количество лотов для входа в лонг
+LotsShort     = 1;      -- Количество лотов для входа в шорт
+TimeStart     = 0;       -- Время начала работы стратегии
+TimeFinish    = 0;      -- Время окончания работы стратегии
 MinutesBuild  = 100;  -- Количество минут построения коробки
 MinutesTrade  = 80;   -- Количество минут поиска входа
-MinHight  = 300;      -- Минимальная высота коробки
-Trailing  = 500;      -- Размер трейлинг-стопа
-StopLoss  = 50;       -- Размер стоп-лосса
--- Остальные переменные скрипта
-DS = nil; -- источник данных (DataSource)
-
--- Поиск в таблице lua
---   TBL - таблица для поиска
---  FUNC - функция сравнения с одним параметром, в который передается значение элемента таблицы. Должна возвращать true в случае совпадения
---  Возвращает ключ и значение первого найденного результата (или nil/nil при неудаче)
-function table_find(tbl,func)    
-  for key,value in pairs(tbl) do        
-    if func(value) then
-      return key,value
-    end    
-  end
-end
-
-function LoadFromFile(fileName)  
-  local file = io.open(fileName, "r")  
-  if file ~= nil then    
-    value = file:read()    
-    file:close()    
-    if value ~= nil then      
-      return value    
-    end  
-  end  
-  return ""
-end
-
-function WriteInFile(fileName, text)  
-  local file = io.open(fileName, "w+t")  
-  if file ~= nil then    
-    file:write(text)    
-    file:close()  
-  end
-end
-
-function AppendInFile(fileName, text)  
-  local file = io.open(fileName, "a")  
-  if file ~= nil then    
-    file:write(text)    
-    file:close()  
-  end
-end
-
--- функция подключения к источнику данных для замены 
-function ConnectData() 
-  --res = CreateDataSource(CLASS_CODE, SEC_CODE, TIME_FRAME); 
-  local f = io.open(getScriptPath().."//data.txt","r");    
-  if f ~= nil then 
-    -- Если файл существует, перебирает строки файла, считывает содержимое в соответствующие переменные  
-    local Count = 0; -- Счетчик строк  
-    for line in f:lines() do   
-      Count = Count + 1;     
-    end; 
-  end; 
-  return res;
-end;
-
--- Параметры источника данных
-CLASS_CODE  = "SPBFUT";     -- Класс бумаги
-SEC_CODE = "RIH7";          -- Код бумаги
-TIME_FRAME = INTERVAL_M1;   -- Таймфрейм
--- Параметры стратегии
-LotsLong   = 1;       -- Количество лотов для входа в лонг
-LotsShort   = 1;      -- Количество лотов для входа в шорт
-TimeStart  = 0;       -- Время начала работы стратегии
-TimeFinish  = 0;      -- Время окончания работы стратегии
-MinutesBuild  = 100;  -- Количество минут построения коробки
-MinutesTrade  = 80;   -- Количество минут поиска входа
-MinHight  = 300;      -- Минимальная высота коробки
-Trailing  = 500;      -- Размер трейлинг-стопа
-StopLoss  = 500;      -- Размер стоп-лосса
+MinHight      = 300;      -- Минимальная высота коробки
+Trailing      = 500;      -- Размер трейлинг-стопа
+StopLoss      = 50;       -- Размер стоп-лосса
 -- Остальные переменные скрипта
 DS = nil; -- источник данных (DataSource)
 
